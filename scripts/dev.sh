1905245085110413db0140d16c3035580e394776
Seth Vargo <seth@sethvargo.com>
2019-11-14 23:42:00 -0500

    Simplify dev script

--- scripts/dev.sh
+++ scripts/dev.sh
@@ -1,8 +1,9 @@
 #!/usr/bin/env bash
-set -e
+set -eEuo pipefail
 
 MNT_PATH="gcp"
 PLUGIN_NAME="vault-plugin-secrets-gcp"
+
 #
 # Helper script for local development. Automatically builds and registers the
 # plugin. Requires `vault` is installed and available on $PATH.
@@ -15,51 +16,37 @@ echo "==> Starting dev"
 
 echo "--> Scratch dir"
 echo "    Creating"
-SCRATCH="$DIR/tmp"
-mkdir -p "$SCRATCH/plugins"
-
-echo "--> Vault server"
-echo "    Writing config"
-tee "$SCRATCH/vault.hcl" > /dev/null <<EOF
-plugin_directory = "$SCRATCH/plugins"
-EOF
-
-echo "    Envvars"
-export VAULT_DEV_ROOT_TOKEN_ID="root"
-export VAULT_ADDR="http://127.0.0.1:8200"
-
-echo "    Starting"
-vault server \
-  -dev \
-  -log-level="debug" \
-  -config="$SCRATCH/vault.hcl" \
-  &
-sleep 2
-VAULT_PID=$!
+SCRATCH="${DIR}/tmp"
+mkdir -p "${SCRATCH}/plugins"
 
 function cleanup {
   echo ""
   echo "==> Cleaning up"
-  kill -INT "$VAULT_PID"
-  rm -rf "$SCRATCH"
+  kill -INT "${VAULT_PID}"
+  rm -rf "${SCRATCH}"
 }
 trap cleanup EXIT
 
-echo "    Authing"
-vault auth root &>/dev/null
-
 echo "--> Building"
-go build -o "$SCRATCH/plugins/$PLUGIN_NAME"
-SHASUM=$(shasum -a 256 "$SCRATCH/plugins/$PLUGIN_NAME" | cut -d " " -f1)
+go build -o "${SCRATCH}/plugins/${PLUGIN_NAME}"
 
-echo "    Registering plugin"
-vault write sys/plugins/catalog/$PLUGIN_NAME \
-  sha_256="$SHASUM" \
-  command="$PLUGIN_NAME"
+echo "--> Starting server"
+
+export VAULT_TOKEN="root"
+export VAULT_ADDR="http://127.0.0.1:8200"
+
+vault server \
+  -dev \
+  -dev-plugin-init \
+  -dev-plugin-dir "${SCRATCH}/plugins" \
+  -dev-root-token-id "root" \
+  -log-level "debug" \
+  &
+sleep 2
+VAULT_PID=$!
 
 echo "    Mouting plugin"
-vault secrets enable -path=$MNT_PATH -plugin-name=$PLUGIN_NAME plugin
+vault secrets enable -path=${MNT_PATH} -plugin-name=${PLUGIN_NAME} plugin
 
 echo "==> Ready!"
-wait $!
-
+wait ${VAULT_PID}
