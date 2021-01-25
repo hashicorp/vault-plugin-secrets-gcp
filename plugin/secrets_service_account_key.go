260702f51108243b0811849b96ee292fbc265492
Yong Wen Chua <lawliet89@users.noreply.github.com>
2021-01-25 10:09:35 +0800

    Add missing checks

--- plugin/secrets_service_account_key.go
+++ plugin/secrets_service_account_key.go
@@ -175,6 +175,12 @@ func (b *backend) secretKeyRevoke(ctx context.Context, req *logical.Request, d *
 
 func (b *backend) getSecretKey(ctx context.Context, s logical.Storage, rs *RoleSet, keyType, keyAlgorithm string, ttl int) (*logical.Response, error) {
 	cfg, err := getConfig(ctx, s)
+	if err != nil {
+		return nil, errwrap.Wrapf("could not read backend config: {{err}}", err)
+	}
+	if cfg == nil {
+		cfg = &config{}
+	}
 	if rs.AccountId == nil {
 		return nil, fmt.Errorf("unable to create secret (service account key), roleset has nil account ID")
 	}
