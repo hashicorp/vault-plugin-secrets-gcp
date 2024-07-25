530702bf134aef49e5cd12189e30b1cb15dd55df
vinay-gopalan <86625824+vinay-gopalan@users.noreply.github.com>
2024-07-25 20:29:23 +0000

    backport of commit b240a5ad808a32c5840bf9cdf4f246f4a626c2be

--- plugin/iamutil/internal/resource_overrides.go
+++ plugin/iamutil/internal/resource_overrides.go
@@ -34,4 +34,61 @@ var resourceOverrides = map[string]map[string]map[string]iamutil.RestResource{
 			},
 		},
 	},
+	"projects/datasets/tables": {
+		"bigquery": {
+			"v2": iamutil.RestResource{
+				Name:                      "tables",
+				TypeKey:                   "projects/datasets/tables",
+				Service:                   "bigquery",
+				IsPreferredVersion:        true,
+				Parameters:                []string{"resource"},
+				CollectionReplacementKeys: map[string]string{},
+				GetMethod: iamutil.RestMethod{
+					HttpMethod: "GET",
+					BaseURL:    "https://bigquery.googleapis.com",
+					Path:       "bigquery/v2/{+resource}:getIamPolicy",
+				},
+				SetMethod: iamutil.RestMethod{
+					HttpMethod: "PATCH",
+					BaseURL:    "https://bigquery.googleapis.com",
+					// NOTE: the bigquery portion of the path needs to be in
+					// the version since googleapis removes it from the
+					// BaseURL when resolving
+					Path:          "bigquery/v2/{+resource}:setIamPolicy",
+					RequestFormat: `{"policy": %s}`,
+				},
+			},
+		},
+	},
+	"projects/datasets/routines": {
+		"bigquery": {
+			"v2": iamutil.RestResource{
+				Name:                      "routines",
+				TypeKey:                   "projects/datasets/routines",
+				Service:                   "bigquery",
+				IsPreferredVersion:        true,
+				Parameters:                []string{"resource"},
+				CollectionReplacementKeys: map[string]string{},
+				GetMethod: iamutil.RestMethod{
+					HttpMethod: "GET",
+					BaseURL:    "https://bigquery.googleapis.com",
+					Path:       "bigquery/v2/{+resource}:getIamPolicy",
+				},
+				SetMethod: iamutil.RestMethod{
+					HttpMethod: "PATCH",
+					BaseURL:    "https://bigquery.googleapis.com",
+					// NOTE: the bigquery portion of the path needs to be in
+					// the version since googleapis removes it from the
+					// BaseURL when resolving
+					Path:          "bigquery/v2/{+resource}:setIamPolicy",
+					RequestFormat: `{"policy": %s}`,
+				},
+			},
+		},
+	},
+}
+
+var resourceSkips = map[string]map[string]struct{}{
+	"poly":            {"v1": {}},      // Advertised as available at https://poly.googleapis.com/$discovery/rest?alt=json&prettyPrint=false&version=v1, but returns a 502
+	"realtimebidding": {"v1alpha": {}}, // Advertised as available at https://realtimebidding.googleapis.com/$discovery/rest?alt=json&prettyPrint=false&version=v1alpha, but returns a 404
 }
